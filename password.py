6358690b00c8a9ffe4cf285e10e35a08d12d6969
John Chung <cckcck@gmail.com>
2018-10-21 00:08:49 +0800

    revised wrong key-in comment

--- password.py
+++ password.py
@@ -1,10 +1,13 @@
-x = 0
+x = 3
 c_password = 'a123456'
-while x < 3 :
+while x > 0 :
+	x -= 1
 	password = input('請輸入密碼: ')
 	if password == c_password:
 		print('登入成功')
 		break
 	else:
-		x += 1
-		print('密碼錯誤! 還有', 3 - x, '次機會')
+		if x > 0:
+			print('密碼錯誤! 還有', x, '次機會')
+		else:
+			print('登入失敗')
